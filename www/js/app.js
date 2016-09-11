64731b874b23bf718f59316d363d7d07e2d4f991
fatihtuzen <fatihtuzen@gmail.com>
2016-09-11 22:49:49 +0300

    ionic share / paylaş nesnesi

---
+++ www/js/app.js
@@ -0,0 +1,23 @@
+﻿angular.module('starter', ['ionic','ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+.controller('shareCtrl', function($scope,$cordovaSocialSharing) {
+
+	$scope.share = function(){
+		$cordovaSocialSharing.share("Bilgilendirme", "Konusu", "dosya yolu", "https://www.google.com");
+	}
+})
